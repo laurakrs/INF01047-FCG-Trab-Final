edc12480add3d6c07a73a8e604fabc3389118f7e
Laura Keidann <laura.keidann@sap.com>
2023-09-03 15:28:10 -0300

    Update bezier

--- Laboratorio_5_Codigo_Fonte/src/bezier.cpp
+++ Laboratorio_5_Codigo_Fonte/src/bezier.cpp
@@ -5,10 +5,11 @@
 // bezier curve - cubica
 glm::vec4 bezierCurve(float t_passed, glm::vec4 p1, glm::vec4 p2, glm::vec4 p3, glm::vec4 p4){
 
-    float t = (t_passed/10.0f) - floor(t_passed/10.0f);
-    glm::vec4 point = (1-t)*(1-t)*(1-t)*p1 +
-            3*t*(1-t)*(1-t)*p2 +
-            3*t*t*(1-t)*p3 +
+    //float t = (t_passed/10.0f) - floor(t_passed/10.0f);
+    float t = fmod(t_passed, 10.0f) / 10.0f; // Normalizado
+    glm::vec4 point = (1.0f-t)*(1.0f-t)*(1.0f-t)*p1 +
+            3.0f*t*(1.0f-t)*(1.0f-t)*p2 +
+            3.0f*t*t*(1.0f-t)*p3 +
             t*t*t*p4;
 
     return point;
