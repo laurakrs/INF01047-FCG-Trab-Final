0fff918d5c0efeeba0a9a0c131cbb226a05a3ccc
Laura Keidann <laura.keidann@sap.com>
2023-09-02 22:28:54 -0300

    update bezier

--- Laboratorio_5_Codigo_Fonte/src/bezier.cpp
+++ Laboratorio_5_Codigo_Fonte/src/bezier.cpp
@@ -5,6 +5,7 @@
 // bezier curve
 glm::vec3 bezierCurve(float t, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3, glm::vec3 p4){
 
+    float t = t - floor(t);
     glm::vec3 point = pow(1.0f-t,3.0f)*p1 +
                     (3.0f * t * pow((1.0f-t),2.0f))*p2 +
                     ((3.0f*pow(t,2.0f) * (1.0f-t))*p3) +
