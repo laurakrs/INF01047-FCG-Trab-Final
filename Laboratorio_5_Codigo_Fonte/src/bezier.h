a22bbbb893589220c4fa93ab1326333a7f3082d6
Laura Keidann <laura.keidann@sap.com>
2023-09-03 00:21:31 -0300

    Update bezier.h

--- Laboratorio_5_Codigo_Fonte/src/bezier.h
+++ Laboratorio_5_Codigo_Fonte/src/bezier.h
@@ -3,14 +3,5 @@
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
 
-// 4 pontos de controle
-//glm::vec4 p1;
-//glm::vec4 p2;
-//glm::vec4 p3;
-//glm::vec4 p4;
-//float t;
-
-//glm::vec4 point;
-
 glm::vec4 bezierCurve(float t_passed, glm::vec4 p1, glm::vec4 p2, glm::vec4 p3, glm::vec4 p4);
 
